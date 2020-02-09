4ab39be8cd4d97643e68d485b0aabea3789dde41
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-02-09 17:57:54 +0700

    add parse method to iterating over search results

--- crawling_e_commerce/spiders/eightwood_crawler.py
+++ crawling_e_commerce/spiders/eightwood_crawler.py
@@ -17,3 +17,37 @@ class EightwoodspiderSpider(scrapy.Spider):
                 # The meta is used to send our search text into the parser as metadata
                 yield scrapy.Request(url, callback = self.parse, meta = {"category_text": category_text})
 
+    def parse(self, response):
+        """Function to process 8wood category results page"""
+        product_category=response.meta["category_text"]
+        products=response.xpath("//div[@class='p-list']")
+        
+        # iterating over search results
+        for product in products:
+            # get produck outstock tag
+            XPATH_PRODUCT_OUTSTOCK_TAG=".//div[@class='img']//span[@class='outstock']"
+            is_onstock = len(product.xpath(XPATH_PRODUCT_OUTSTOCK_TAG)) == 0
+
+            # get data if product is onstock
+            if is_onstock: 
+                # Defining the XPaths
+                XPATH_PRODUCT_NAME=".//div[@class='desc']//h5/a/text()"
+                XPATH_PRODUCT_PRICE=".//div[@class='desc']//div[@class='price-box']//span[@class='regular-price']//span[@class='price']/text()"
+                XPATH_PRODUCT_IMAGE_LINK=".//div[@class='img']//a[@class='product-image-custom']//img[contains(@class,'imgThumProduct')]/@data-original"
+                XPATH_PRODUCT_LINK=".//div[@class='desc']//h5/a/@href"
+
+                raw_product_name=product.xpath(XPATH_PRODUCT_NAME).extract()
+                raw_product_price=product.xpath(XPATH_PRODUCT_PRICE).extract()
+                raw_product_image_link=product.xpath(XPATH_PRODUCT_IMAGE_LINK).extract()
+                raw_product_link=product.xpath(XPATH_PRODUCT_LINK).extract()
+                
+                # cleaning the data
+                product_name=''.join(raw_product_name).strip(
+                ) if raw_product_name else None
+                product_price=''.join(raw_product_price).strip(
+                ) if raw_product_price else None
+                product_image_link=''.join(raw_product_image_link).strip(
+                ) if raw_product_image_link else None
+                product_link=str(EightwoodspiderSpider.start_urls[0])+''.join(raw_product_link).strip(
+                ) if raw_product_link else None
+                
