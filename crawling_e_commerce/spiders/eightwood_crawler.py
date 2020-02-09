a9cc9050bec21aad6679d01a696f5eb78fa74b0a
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-02-09 17:56:44 +0700

    Read category_text from eightwood_categories file and construct the URL

--- crawling_e_commerce/spiders/eightwood_crawler.py
+++ crawling_e_commerce/spiders/eightwood_crawler.py
@@ -8,4 +8,12 @@ class EightwoodspiderSpider(scrapy.Spider):
     start_urls = ['http://www.8wood.id']
 
     def start_requests(self):
-        
+         """Read category_text from eightwood_categories file and construct the URL"""
+
+        with open(os.path.join(os.path.dirname(__file__), "../resources/eightwood_categories.csv")) as categories:
+            for category in csv.DictReader(categories):
+                category_text=category["category"]
+                url=str(EightwoodspiderSpider.start_urls[0])+"/pakaian/"+category_text+"/"
+                # The meta is used to send our search text into the parser as metadata
+                yield scrapy.Request(url, callback = self.parse, meta = {"category_text": category_text})
+
